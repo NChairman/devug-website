d9769a2ff997764f1acb1455e99971d72152993c
Thabiso Magwaza <magwazathabiso9@gmail.com>
2025-07-20 14:11:01 +0200

    fix: format landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,8 +6,11 @@ import { api } from '../../convex/_generated/api';
 export default function Home() {
   const tasks = useQuery(api.tasks.get);
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      {tasks?.map(({ _id, text }) => <div key={_id}>{text}</div>)}
+    <main className="flex flex-col items-center justify-between p-24 gap-4">
+      <h1 className="text-2xl font-bold">Tasks</h1>
+      <ol className="list-decimal list-inside">
+        {tasks?.map(({ _id, text }) => <li key={_id}>{text}</li>)}
+      </ol>
     </main>
   );
 }
